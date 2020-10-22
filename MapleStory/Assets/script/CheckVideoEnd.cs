a3138a542912f71e0f86038211bbeab69427e142
gic91 <43128286+gic91@users.noreply.github.com>
2020-10-22 21:35:44 +0900

    10/22 프로젝트 뼈대 / 인트로영상

---
+++ MapleStory/Assets/script/CheckVideoEnd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CheckVideoEnd : MonoBehaviour
+{
+   public VideoPlayer intro;
+   void Awake () 
+   {
+      intro = GetComponent<VideoPlayer>();
+      intro.loopPointReached += OnMovieFinished; // loopPointReached is the event for the end of the video
+   }
+
+   void OnMovieFinished(VideoPlayer player)
+   {
+      Debug.Log("Event for movie end called");
+      player.Stop();
+      SceneManager.LoadScene("1_MenuScene");
+   }
+}
